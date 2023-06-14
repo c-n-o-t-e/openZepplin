3caf606f67a238d5e7ebd30f5e0b65905bd4fd7e
c-note <dongustavo20@gmail.com>
2023-06-14 20:45:12 +0100

    created admitStudent and gradeStudent functions

--- src/AccessControlInteraction.sol
+++ src/AccessControlInteraction.sol
@@ -6,8 +6,33 @@
  * @title Claim
  * @author: c-n-o-t-e
  */
+
 pragma solidity ^0.8.13;
 
-contract AccessControlInteraction {
-    
+import {AccessControl} from "openzeppelin-contracts/contracts/access/AccessControl.sol";
+
+contract AccessControlInteraction is AccessControl {
+
+    bytes32 public constant ADMISSION_OFFICER = keccak256("ADMISSION_OFFICER");
+
+    constructor() {
+        _grantRole(DEFAULT_ADMIN_ROLE, _msgSender());
+    }
+
+    struct StudentInfo{
+        string name;
+        string email;
+    }
+
+    mapping(address => StudentInfo) public students;
+
+    function admitStudent(string memory _name, string memory _email) external onlyRole(ADMISSION_OFFICER) {
+        StudentInfo storage student = students[_msgSender()];
+        student.name = _name;
+        student.email = _email;
+    }
+
+    function gradeStudent(address _studentAddress, uint _grade) external {
+        
+    }
 }
