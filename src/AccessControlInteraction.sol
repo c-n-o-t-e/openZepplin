b6e2fa00b40ab307c31f61787d599eb025b7f093
c-note <dongustavo20@gmail.com>
2023-06-14 20:02:07 +0100

    created contract to test accessControl

---
+++ src/AccessControlInteraction.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+
+/**
+ * Created on 2023-06-14 19:08
+ * @Summary A smart contract that lets adminstration grant grades based on the role they hold
+ * @title Claim
+ * @author: c-n-o-t-e
+ */
+pragma solidity ^0.8.13;
+
+contract AccessControlInteraction {
+    
+}
