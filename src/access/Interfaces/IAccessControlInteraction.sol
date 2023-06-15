b73b05ebd831c7089acb46978d18df0b319e5794
c-note <dongustavo20@gmail.com>
2023-06-15 05:07:14 +0100

    created interface for AccessControlInteraction

---
+++ src/access/Interfaces/IAccessControlInteraction.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: UNLICENSED
+
+/**
+ * @dev External interface of AccessControlInteraction.
+ */
+interface IAccessControlInteraction {
+    /**
+     * @dev Emits when a new student is admitted.
+     */
+    event NewStudent(string _name, string _email);
+
+    /**
+     * @dev Emits when a student gets graded.
+     */
+    event StudentGrade(address _studentAddress, uint256 _grade);
+
+    function admitStudent(string memory _name, string memory _email) external;
+
+    function gradeStudent(address _studentAddress, uint _grade) external;
+}
